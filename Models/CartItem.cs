5866b95dd733bd7acc328e8e2b58564fabab7bdf
Shakhida Baudunova <baudunova_s@auca.kg>
2026-09-24 21:43:53 +0600

    Initial product catalog implementation

---
+++ Models/CartItem.cs
@@ -0,0 +1,8 @@
+namespace ProductCatalog.Models;
+
+public class CartItem
+{
+    public Product Product { get; set; } = new Product();
+
+    public int Quantity { get; set; } = 1;
+}
---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+namespace ProductCatalog.Models;
+
+public class Product
+{
+    public string Name { get; set; } = "";
+    public decimal Price { get; set; }
+    public string Category { get; set; } = "";
+}
